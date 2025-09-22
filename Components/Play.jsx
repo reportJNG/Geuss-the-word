2ca2156bc00bc673cefc4ecd0871ebf89ecae4c8
Hamza Remali <Hamzaremali10@gmail.com>
2025-09-21 16:17:10 -1100

    we

--- Components/Play.jsx
+++ Components/Play.jsx
@@ -1,6 +1,14 @@
+import { useState } from "react"
 import styles from "./Play.module.css"
 
 export default function Play() {
+  [Name,setName]=useState('');
+  const Play = () =>{
+    if (Name.lenght < 3 || Name.lenght >26 ) { 
+      setName('Player');
+     }
+    
+  }
   return (
     <>
       <div className={styles.container}>
@@ -8,8 +16,12 @@ export default function Play() {
           <h1>Ready to Challenge Your Mind?</h1>
           <p>Test your vocabulary skills and unlock the mystery word!</p>
         </div>
+        <div className={styles.Inp}>
+          <input type="text" required maxLength={26} minLength={3} value={Name} placeholder="Name ..."/>
+        </div>
         <div className={styles.btn}>
-          <button className={styles.playButton}>
+          <button className={styles.playButton}   onClick={Play}>
+
             <span className={styles.playIcon}>▶</span>
             <span className={styles.playText}>Start Game</span>
           </button>
